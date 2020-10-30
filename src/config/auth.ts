226add95f3cdd697c4c1d24b81fb5ab89a1b9817
deal <95deal@gmail.com>
2020-10-30 11:38:49 -0300

    first commit but not starting

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: "bfeb6e441b18e03d5d86100406a65fd3",
+    expiresIn: "1d",
+  },
+};
---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import routes from "./routes/index";
+import "./database/index";
+import "reflect-metadata";
+
+const app = express();
+
+app.use(express.json());
+
+app.use(routes);
+
+app.listen(3333, () => {
+  console.log("server started on port 3333");
+});
