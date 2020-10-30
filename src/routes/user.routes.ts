cb900b7bf9facc59975a24ef1b61ba46cf9bf077
deal <95deal@gmail.com>
2020-10-30 11:56:11 -0300

    create route of users avatar

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,7 +1,12 @@
 import { Router } from "express";
+import multer from "multer";
+
 import CreateUserService from "../services/CreateUserService";
 import ensureAuthenticated from "../middlewares/ensureAuthenticated";
+import uploadConfig from "../config/upload";
+
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post("/", async (request, response) => {
   try {
@@ -21,7 +26,14 @@ usersRouter.post("/", async (request, response) => {
   }
 });
 
-usersRouter.patch("/avatar", ensureAuthenticated, async (request, response) => {
-  return response.json({ ok: false });
-});
+usersRouter.patch(
+  "/avatar",
+  ensureAuthenticated,
+  upload.single("avatar"),
+  async (request, response) => {
+    console.log(request.file);
+
+    return response.json({ ok: false });
+  }
+);
 export default usersRouter;
